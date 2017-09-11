f641e11b69b0b10afdd1a13ac2d34fa55387f5b1
Michael Marshal <marshal.2015085@iit.ac.lk>
2017-09-11 19:16:57 +0530

    Added our first viewset and router.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -61,6 +61,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet """
 
+    serializer_class = serializers.HelloSerializer
+
     def list(self, request):
         """Return a hello message."""
 
@@ -71,4 +73,36 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message':'Hello!','a_viewset':a_viewset})
-        
+
+    def create(self, request):
+        """Create a new hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message= 'Hello {0}'.format(name)
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+    def retireve(self, request):
+        """ Handels getting an object by its ID. """
+
+        return Rsponse({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating aand object."""
+
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ Handles updating part of an objet """
+
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handles removing and object."""
+
+        return Response({'http_method':'DELETE'})
